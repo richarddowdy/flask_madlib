5a139f706e4afd586227dd7b46aadbe1d879c0ce
David Sommers <davidcsommers@gmail.com>
2020-01-22 15:49:10 -0800

    Using jinja and templates now.

--- app.py
+++ app.py
@@ -1,29 +1,32 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from stories import story
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/story")
 def root():
     """Show Homepage"""
-    input_form = ""
-    for prompt in story.prompts:
-        input_form += f'<label style="display:block">{prompt} : <input  name="{prompt}""></input></label>'
 
-    return f"""
-        <html>
-            <head>
-                <title>Madlibs</title>
-            </head>
-            <body>
-                <h1>Madlibs</h1>
-                <form action="/story" method="POST">
-                    {input_form}
-                    <button>SUBMIT</button>
-                </form>
-            </body>
-        </html>
-    """
+    return render_template("madLibPrompt.html", prompts=story.prompts)
+
+    # input_form = ""
+    # for prompt in story.prompts:
+    #     input_form += f'<label style="display:block">{prompt} : <input  name="{prompt}""></input></label>'
+
+    # return f"""
+    #     <html>
+    #         <head>
+    #             <title>Madlibs</title>
+    #         </head>
+    #         <body>
+    #             <h1>Madlibs</h1>
+    #             <form action="/story" method="POST">
+    #                 {input_form}
+    #                 <button>SUBMIT</button>
+    #             </form>
+    #         </body>
+    #     </html>
+    # """
 
 @app.route("/story", methods=["POST"])
 def madlib_story():
@@ -31,7 +34,4 @@ def madlib_story():
 
     answers = request.form
 
-    return f"<h1>{story.generate(answers)}</h1>"
-
-    
-
+    return render_template("madLibStory.html", your_story=story.generate(answers))
